85e38789d19f19fc3401d3f6395408eba09b5ecb
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-11-30 12:54:26 +0300

    Ogranicivajem castotu kadrov.

--- src/Host.cpp
+++ src/Host.cpp
@@ -19,9 +19,12 @@ Host::Host()
 bool Host::Loop()
 {
 	const Clock::time_point tick_start_time= Clock::now();
+	const auto dt= tick_start_time - prev_tick_time_;
 	prev_tick_time_ = tick_start_time;
 
 	const float time_s= float((tick_start_time - init_time_).count()) * float(Clock::duration::period::num) / float(Clock::duration::period::den);
+
+	const float dt_s= float(dt.count()) * float(Clock::duration::period::num) / float(Clock::duration::period::den);
 
 	const SystemEvents system_events= system_window_.ProcessEvents();
 	for(const SystemEvent& system_event : system_events)
@@ -38,6 +41,17 @@ bool Host::Loop()
 	window_opengl_.BeginFrame();
 	game_launcher_.RunFrame(time_s);
 	window_opengl_.EndFrame();
+
+	const Clock::time_point tick_end_time= Clock::now();
+	const auto frame_dt= tick_end_time - tick_start_time;
+
+	const float max_fps= 120.0f;
+
+	const std::chrono::milliseconds min_frame_duration(uint32_t(1000.0f / max_fps));
+	if(frame_dt <= min_frame_duration)
+	{
+		std::this_thread::sleep_for(min_frame_duration - frame_dt);
+	}
 
 	return false;
 }
